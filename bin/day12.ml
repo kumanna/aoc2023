cbf5743c7d8b0cc1b7be116bbd2c4fb3f7fb2011
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2024-11-19 15:36:23 +0530

    Day 12 part 2 (incomplete)

--- bin/day12.ml
+++ bin/day12.ml
@@ -17,6 +17,8 @@ let get_question_indices s =
   in
   get_question_indices_helper s 0 [] |> List.rev
 
+let eh_hashtable = Hashtbl.create 16
+
 let extract_hash_strings s =
   let rec extract_hash_strings_helper s previous_hash running_list =
     if String.length s < 1 then running_list
@@ -35,22 +37,42 @@ let extract_hash_strings s =
           extract_hash_strings_helper rest_str false running_list
         else extract_hash_strings_helper rest_str false running_list
   in
-  (extract_hash_strings_helper s false []) |> List.rev
+  let retval =
+    try Hashtbl.find eh_hashtable s
+    with Not_found ->
+      (extract_hash_strings_helper s false []) |> List.rev
+  in
+  retval
+
 
 let cartesian_product l1 l2 =
   l1 |> List.map (fun x -> (x, l2)) |> List.map (fun (x, y) -> (List.map (fun z -> List.concat [x; z]) y)) |> List.concat
 
+let cp_hashtable = Hashtbl.create 16
+
 let rec nth_cartesian_product n l =
-  if n = 0 then []
-  else if n = 1 then
-    [["#"];["."]]
-  else if n = 2 then cartesian_product [["#"];["."]] l
-  else nth_cartesian_product (n - 1) (cartesian_product [["#"];["."]] l)
+  let retval =
+    try Hashtbl.find cp_hashtable (n, l)
+    with Not_found ->
+      if n = 0 then []
+      else if n = 1 then
+        [["#"];["."]]
+      else if n = 2 then cartesian_product [["#"];["."]] l
+      else nth_cartesian_product (n - 1) (cartesian_product [["#"];["."]] l)
+  in
+  retval
+
+let q_hashtable = Hashtbl.create 16
 
 let rec build_unquestioned_string s fill_vals =
-  match String.index_opt s '?' with
-  | Some n -> build_unquestioned_string ((String.sub s 0 n) ^ (List.hd fill_vals) ^ (String.sub s (n + 1) ((String.length s) - n - 1))) (List.tl fill_vals)
-  | None -> s
+  let retval =
+    try Hashtbl.find q_hashtable (s, fill_vals)
+    with Not_found ->
+    match String.index_opt s '?' with
+    | Some n -> build_unquestioned_string ((String.sub s 0 n) ^ (List.hd fill_vals) ^ (String.sub s (n + 1) ((String.length s) - n - 1))) (List.tl fill_vals)
+    | None -> s
+  in
+  retval
 
 let () =
   let lines = read_lines file in
