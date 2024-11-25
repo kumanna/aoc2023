a34d447ad057a4dc6c203c01071dbac8747f48c4
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2024-11-25 10:42:56 +0530

    Add day 13 part 2

--- bin/day13.ml
+++ bin/day13.ml
@@ -23,6 +23,40 @@ let transpose a =
   done;
   at
 
+let check_string_difference a b =
+  let rec check_string_difference_helper a b n =
+    if (String.length a) = 0 || (String.length b) = 0 then
+      n
+    else
+      let (a_begin, a_end) = (String.get a 0, String.sub a 1 ((String.length a) - 1)) in
+      let (b_begin, b_end) = (String.get b 0, String.sub b 1 ((String.length b) - 1)) in
+      if a_begin = b_begin then
+        check_string_difference_helper a_end b_end n
+      else
+        check_string_difference_helper a_end b_end (n + 1)
+  in
+  check_string_difference_helper a b 0
+
+let check_pattern_symmetry_smudge a i =
+  let return_value = ref true in
+  let smudge_count = ref 0 in
+  let l = Array.length a in
+  let (start_index, stop_index) =
+    if i <= l / 2 then
+      (0, i - 1)
+    else
+      (i - (l - i), i - 1)
+  in
+  for k = start_index to stop_index do
+    let string_diff = check_string_difference a.(k) a.(i - k + i - 1) in
+    return_value := (!return_value) && (string_diff < 2);
+    smudge_count := (!smudge_count) + string_diff
+  done;
+  if !smudge_count = 1 then
+    !return_value
+  else
+    false
+
 let check_pattern_symmetry a i =
   let return_value = ref true in
   let l = Array.length a in
@@ -46,15 +80,28 @@ let find_pattern_symmetry a =
   done;
   !symmetry_count
 
+let find_pattern_symmetry_smudge a =
+  let l = Array.length a in
+  let symmetry_count = ref 0 in
+  for i = 1 to l - 1 do
+    if check_pattern_symmetry_smudge a i then
+      symmetry_count := i
+  done;
+  !symmetry_count
+
 let () =
   let lines = read_lines file in
   let split_pattern = List.fold_left (fun x y -> convert_to_list x y) [] lines in
-  split_pattern
-  |> List.filter (fun x -> List.length x > 0)
-  |> List.rev
-  |> List.map Array.of_list
-  |> List.map (fun x -> (find_pattern_symmetry x, find_pattern_symmetry (transpose x)))
-  |> List.map (fun (x, y) -> 100 * x + y)
-  |> List.fold_left (fun x y -> x + y) 0
-  |> string_of_int
-  |> print_endline
+  let find_summary symmetry_func =
+    split_pattern
+    |> List.filter (fun x -> List.length x > 0)
+    |> List.rev
+    |> List.map Array.of_list
+    |> List.map (fun x -> (symmetry_func x, symmetry_func (transpose x)))
+    |> List.map (fun (x, y) -> 100 * x + y)
+    |> List.fold_left (fun x y -> x + y) 0
+    |> string_of_int
+    |> print_endline;
+  in
+  find_summary find_pattern_symmetry;
+  find_summary find_pattern_symmetry_smudge;
